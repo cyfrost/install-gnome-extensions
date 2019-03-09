736565ff387eb29d0fa9a0925551a419c9f2155e
Cyrus Frost <cyrus.frost@hotmail.com>
2019-03-09 17:46:34 +0530

    format

--- install_gnome_extensions.sh
+++ install_gnome_extensions.sh
@@ -22,65 +22,59 @@ gnome_shell_version="$(gnome-shell --version | cut --delimiter=' ' --fields=3 | 
 # sudo dnf install wget curl jq unzip -y
 # sudo apt install wget curl jq unzip -y
 
-install_shell_extensions(){	
+install_shell_extensions(){
 
+    for ext_id in "${extension_IDs_to_install[@]}"; do
 
+        request_url="https://extensions.gnome.org/extension-info/?pk=$ext_id&shell_version=$gnome_shell_version";
 
+        http_response_header="$(curl -s -o /dev/null -I -w "%{http_code}" $request_url)";
 
- 
-
-
-        for ext_id in "${extension_IDs_to_install[@]}"; do	
-          
-          request_url="https://extensions.gnome.org/extension-info/?pk=$ext_id&shell_version=$gnome_shell_version";
-
-          http_response_header="$(curl -s -o /dev/null -I -w "%{http_code}" $request_url)";
-
-          if [ $http_response_header = 404 ]; then
+        if [ $http_response_header = 404 ]; then
             printf "\n${error_text}No extension exists with the given ID $ext_id (Skipping this).\n";
             continue;
-          fi
-          
-          printf "${normal_text}\n";
-          ext_info="$(curl -s $request_url)";
-          extension_name="`echo $ext_info | jq -r .name`";	
-          direct_dload_url="`echo $ext_info | jq -r '.download_url'`";
-          ext_uuid="`echo $ext_info | jq -r '.uuid'`";
-          ext_version="`echo $ext_info | jq -r '.version'`";
-          download_url="https://extensions.gnome.org"$direct_dload_url;	
-          filename=$(basename $download_url);
-          target_installation_dir="/home/$USER/.local/share/gnome-shell/extensions/$ext_uuid";
-          printf "${status_text}\nDownloading and installing \"$extension_name\"${normal_text}";
-          printf "${info_text}"
-          printf "\nExtension ID: $ext_id";
-          printf "\nExtension Version: v$ext_version";
-          printf "\nURL: $download_url";
-          printf "\nUUID: \"$ext_uuid\"";
-          #printf "\nFilename: $filename";
-          printf "\nInstalling to: \"$target_installation_dir\"";
-          printf "\nPlease wait..."
-          wget -q $download_url;
-          mkdir -p $target_installation_dir;
-          unzip -o -q $filename -d $target_installation_dir;
-          rm $filename;
-          printf "\nDone!\n";
-          printf "${normal_text}"
-    done	
+        fi
+
+        printf "${normal_text}\n";
+        ext_info="$(curl -s $request_url)";
+        extension_name="`echo $ext_info | jq -r .name`";
+        direct_dload_url="`echo $ext_info | jq -r '.download_url'`";
+        ext_uuid="`echo $ext_info | jq -r '.uuid'`";
+        ext_version="`echo $ext_info | jq -r '.version'`";
+        download_url="https://extensions.gnome.org"$direct_dload_url;
+        filename=$(basename $download_url);
+        target_installation_dir="/home/$USER/.local/share/gnome-shell/extensions/$ext_uuid";
+        printf "${status_text}\nDownloading and installing \"$extension_name\"${normal_text}";
+        printf "${info_text}"
+        printf "\nExtension ID: $ext_id";
+        printf "\nExtension Version: v$ext_version";
+        printf "\nURL: $download_url";
+        printf "\nUUID: \"$ext_uuid\"";
+        #printf "\nFilename: $filename";
+        printf "\nInstalling to: \"$target_installation_dir\"";
+        printf "\nPlease wait..."
+        wget -q $download_url;
+        mkdir -p $target_installation_dir;
+        unzip -o -q $filename -d $target_installation_dir;
+        rm $filename;
+        printf "\nDone!\n";
+        printf "${normal_text}"
+    done
     printf "\n";
 }
 
 printf "\n${status_text}Welcome. This script allows you to install your favourite GNOME Shell extensions with ease of use.\n${normal_text}";
 
- if [ ${#extension_IDs_to_install[@]} -eq 0 ]; then
+if [ ${#extension_IDs_to_install[@]} -eq 0 ]; then
+
     printf "\n${info_text}No extension IDs have been specified for installation.\nPlease open this script in a text editor to specify the extension IDs for installation.${normal_text}\n\n";
-    
-  else
 
-printf "\nGNOME Shell version detected: $gnome_shell_version\nStarting installation...\n";
+else
 
-install_shell_extensions;
+    printf "\nGNOME Shell version detected: $gnome_shell_version\nStarting installation...\n";
 
-printf "${status_text}Finished!\nExtensions were successfully installed but not enabled, you can enable them by using the GNOME Tweak Tool application.\n\n${normal_text}";
+    install_shell_extensions;
 
+    printf "${status_text}Finished!\nExtensions were successfully installed but not enabled, you can enable them by using the GNOME Tweak Tool application.\n\n${normal_text}";
 
 fi
