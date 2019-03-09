887e67247e27c23737fec44dd1d8e1c42e3b7c0b
Cyrus Frost <cyrus.frost@hotmail.com>
2019-03-09 17:40:55 +0530

    Add null check

--- install_gnome_extensions.sh
+++ install_gnome_extensions.sh
@@ -7,7 +7,7 @@
 # To obtain the ID of an extension you want to install, simply look for the number in its extension URL page. For example, the ID of the popular "User Themes" extension is 19, which is directly visible in it's URL: https://extensions.gnome.org/extension/19/user-themes/.
 
 # You can specify the IDs of all the extensions you want to install in the below array (space delimited). In the default example, I've added the 3 ids of the most popular extensions as a sample.
-extension_IDs_to_install=(6 8 19)
+extension_IDs_to_install=(6)
 
 # Defining text colors for statuses.
 info_text=$(tput setaf 4);
@@ -23,6 +23,13 @@ gnome_shell_version="$(gnome-shell --version | cut --delimiter=' ' --fields=3 | 
 # sudo apt install wget curl jq unzip -y
 
 install_shell_extensions(){	
+
+
+
+
+ 
+
+
         for ext_id in "${extension_IDs_to_install[@]}"; do	
           
           request_url="https://extensions.gnome.org/extension-info/?pk=$ext_id&shell_version=$gnome_shell_version";
@@ -64,8 +71,16 @@ install_shell_extensions(){
 
 printf "\n${status_text}Welcome. This script allows you to install your favourite GNOME Shell extensions with ease of use.\n${normal_text}";
 
+ if [ ${#extension_IDs_to_install[@]} -eq 0 ]; then
+    printf "\n${info_text}No extension IDs have been specified for installation.\nPlease open this script in a text editor to specify the extension IDs for installation.${normal_text}\n\n";
+    
+  else
+
 printf "\nGNOME Shell version detected: $gnome_shell_version\nStarting installation...\n";
 
 install_shell_extensions;
 
 printf "${status_text}Finished!\nExtensions were successfully installed but not enabled, you can enable them by using the GNOME Tweak Tool application.\n\n${normal_text}";
+
+
+fi
