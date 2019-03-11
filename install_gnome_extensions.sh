4852a960c2131ad40f9a17a009241e5afb6c3d99
Cyrus Frost <cyrus.frost@hotmail.com>
2019-03-11 20:14:33 +0530

    add dep helper commands and message for foreign DE

--- install_gnome_extensions.sh
+++ install_gnome_extensions.sh
@@ -8,16 +8,11 @@
 # Author: Cyfrost
 # ========================================================================================================
 
-
-# message colors.
-info_text_blue=$(tput setaf 4);
-normal_text=$(tput sgr0);
-error_text=$(tput setaf 1);
-status_text_yellow=$(tput setaf 3);
-
 #vars
 args_count="$#"
-dependencies=(wget curl jq unzip gnome-shell-extension-tool gnome-shell cut basename)
+dependencies=(wget curl jq unzip tput sed gnome-shell-extension-tool gnome-shell cut basename)
+deps_install_apt="sudo apt install -y wget curl jq unzip sed"
+deps_install_dnf="sudo dnf install -y wget curl jq unzip sed"
 EXTENSIONS_TO_INSTALL=()
 OVERWRITE_EXISTING=false
 ENABLE_ALL=false
@@ -30,12 +25,24 @@ function CheckIfRunningAsRoot(){
     fi
 }
 
+# Bail immediately if running as root.
+CheckIfRunningAsRoot
+
+# message colors.
+info_text_blue=$(tput setaf 2);
+normal_text=$(tput sgr0);
+error_text=$(tput setaf 1);
+status_text_yellow=$(tput setaf 3);
+
 # Trap SIGINT and SIGTERM.
 function _term() {
     printf "\n\n${normal_text}";
     trap - INT TERM # clear the trap
     kill -- -$$
 }
+
+# Trap SIGINT and SIGTERM for cleanup.
+trap _term INT TERM
 
 # This function can check for binaries/commands to be available in Env PATH and report otherwise.
 function CheckDependencies(){
@@ -46,19 +53,39 @@ function CheckDependencies(){
     do
         command -v "$name" >/dev/null 2>&1 || { echo -en "${error_text}\nError: command not found: $name${normal_text}";deps=1; }
     done
-    [[ $deps -ne 1 ]] && echo "${info_text_blue}OK${normal_text}" || { echo -en "${error_text}\n\nPlease make sure the above commands are available and re-run this script.\n\n${normal_text}";exit 1; }
+    [[ $deps -ne 1 ]] && echo "${info_text_blue}OK${normal_text}" || { echo -en "${error_text}\n\nOne or more dependencies is unavailable. Please make sure the above commands are available and re-run this script.\n\n${status_text_yellow}For Ubuntu and similar distros, try: $deps_install_apt\n\nFor Fedora and similar distros, try: $deps_install_dnf\n\n${normal_text}";exit 1; }
 }
+
+# Fail if dependencies unmet.
+CheckDependencies "${dependencies[@]}"
 
 function confirm_action() {
     while true; do
         printf "\n${normal_text}";
-        read -rep "$1" -n 1 yn
+        read -p "$1" -n 1 yn
         case $yn in
             [Yy]* ) return 0;;
             [Nn]* ) return 1;;
             * ) echo "Please answer with 'y' or 'n'.";;
         esac
     done
+}
+
+function IsEnvGNOME(){
+
+    if [ "$XDG_CURRENT_DESKTOP" = "" ]; then
+        desktop=$(echo "$XDG_DATA_DIRS" | sed 's/.*\(xfce\|kde\|gnome\).*/\1/')
+    else
+        desktop=$XDG_CURRENT_DESKTOP
+    fi
+
+    desktop=${desktop,,}
+ 
+    if [[ $desktop == *"gnome"* ]]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 function enable_extension(){
@@ -93,7 +120,7 @@ function install_shell_extensions(){
         ext_description="$(echo "$ext_info" | jq -r '.description')";
         download_url="https://extensions.gnome.org"$direct_dload_url;
         target_installation_dir="/home/$USER/.local/share/gnome-shell/extensions/$ext_uuid";
-        printf "${status_text_yellow}\nDownloading and installing \"$extension_name\"${normal_text}";
+        printf "${status_text_yellow}\nDownloading and installing extension \"$extension_name\"${normal_text}";
         printf "${info_text_blue}"
         printf "\nDescription: $ext_description";
         printf "\nExtension ID: $ext_id";
@@ -103,9 +130,7 @@ function install_shell_extensions(){
         printf "\nInstalling to: \"$target_installation_dir\"";
 
         if [ -d "$target_installation_dir" ]  && [ "$OVERWRITE_EXISTING" = "false" ]; then
-            confirm_action "${normal_text}This extension is already installed. Would you like to overwrite it? (y/n): " || continue;
-        else
-            printf "\n"
+            confirm_action "${normal_text}This extension is already installed. Do you want to overwrite it? (y/n): " && printf "\n" || continue;
         fi
 
         printf "${info_text_blue}Please wait..."
@@ -118,9 +143,11 @@ function install_shell_extensions(){
 
         if [ "$ENABLE_ALL" = "false" ]; then
             confirm_action "${normal_text}Enable this extension now? (y/n): " && enable_extension "$ext_uuid";
+        else
+            enable_extension "$ext_uuid"
         fi
         
-        printf "${info_text_blue}Done!\n";
+        printf "\n${info_text_blue}Done!\n";
         printf "${normal_text}"
     done
     printf "\n";
@@ -136,27 +163,20 @@ function IsNumber(){
 }
 
 function print_usage(){
-    printf "\n${normal_text}Usage: ./install_gnome_extensions.sh <extension_id1> <extension_id2> <extension_id3> ...\n\nExample usage: ./install_gnome_extensions.sh 6 8 19\n\n";
+    printf "\n${normal_text}Usage: ./install_gnome_extensions.sh [options] <extension_ids>\n\nOptions:\n\t--enable\tEnables the extension after downloading and installing it.\n\t--update\tUpdates existing extensions to latest available versions.\n\t--overwrite\tOverwrites existing extensions, disabled by default.\n\nExample usage: ./install_gnome_extensions.sh 6 8 19 --enable\n\n";
 }
 
 function begin_install(){
 
     printf "\nGNOME Shell version detected: $GNOME_SHELL_VERSION\nStarting installation for $extensions_count extensions...\n";
     install_shell_extensions;
-    printf "${normal_text}Complete!\n\n$";
+    printf "\n${normal_text}Complete!\n\n";
+    IsEnvGNOME || printf "${normal_text}Please login to GNOME desktop to see the installed/enabled extensions.\n\n"
 }
 
-# Bail immediately if running as root.
-CheckIfRunningAsRoot
-
-# Fail if dependencies unmet.
-CheckDependencies "${dependencies[@]}"
 
 # Obtain GNOME Shell version.
 GNOME_SHELL_VERSION="$(gnome-shell --version | cut --delimiter=' ' --fields=3 | cut --delimiter='.' --fields=1,2)";
-
-# Trap SIGINT and SIGTERM for cleanup.
-trap _term INT TERM
 
 printf "${status_text_yellow}\n================================\nGNOME Shell Extensions Installer\n================================\n\nThis script allows you to install your favourite GNOME Shell extensions with ease of use.\n\nHomepage: https://github.com/cyfrost/install-gnome-extensions${normal_text}\n";
 
@@ -164,16 +184,13 @@ printf "${status_text_yellow}\n================================\nGNOME Shell Ext
 while test $# -gt 0; do
     case "$1" in
         --enable)
-            echo "extensions will be enabled upon install"
-    		ENABLE_ALL=true
+            ENABLE_ALL=true
             ;;
         --update) 
-		   	echo "extensions will be updated to latest available versions"
-		    UPDATE=true
+		   	UPDATE=true
             ;;
         --overwrite) 
-		    echo "extensions will be overwritten if no newer version is found"
-			OVERWRITE_EXISTING=true
+		    OVERWRITE_EXISTING=true
             ;;
     esac
     IsNumber "$1" && EXTENSIONS_TO_INSTALL+=($1)
